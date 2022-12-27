2d293236e1a1076a5c51b25625433bad2013b327
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2022-12-28 00:00:16 +0100

    feat: SSG props with typescript types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,32 @@
 import Head from "next/head";
+import { GetStaticProps } from "next";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      content: {
+        title: "Meet Stefan Hoth - A Senior Engineering Leader",
+        description:
+          "Stefan Hoth is an experienced Engineering Leader based in Germany.",
+        welcome: "Hi from Next.js 13 👋👋👋",
+      },
+    }, // will be passed to the page component as props
+  };
+};
+
+export default function Home({ content }) {
   return (
     <>
       <Head>
-        <title>Meet Stefan Hoth - A Senior Engineering Leader</title>
-        <meta
-          name="description"
-          content="Stefan Hoth is an experienced Engineering Leader based in Germany."
-        />
+        <title>{content.title}</title>
+        <meta name="description" content="{content.description}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>
-          <p>
-            Hi from Next.js 13 👋👋👋
-          </p>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <h1>{content.welcome}</h1>
         </div>
       </main>
     </>
