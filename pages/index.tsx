6debaa67bafec611283b868d1ff3cb9135eee1e5
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2022-12-28 21:56:40 +0100

    feat: First step of static content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Head from "next/head";
 import { GetStaticProps } from "next";
 import styles from "../styles/Home.module.css";
+import avatarPic from "../public/profile.png";
+import Image, { StaticImageData } from "next/image";
 
 interface ContentProps {
   title: string;
   description: string;
   welcome: string;
   subline: string;
+  avatar: StaticImageData;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -14,8 +17,9 @@ export const getStaticProps: GetStaticProps = async () => {
     title: "Meet Stefan Hoth - A Senior Engineering Leader",
     description:
       "Stefan Hoth is an experienced Engineering Leader based in Germany.",
-    welcome: "Hi from Next.js 13 👋👋👋",
-    subline: "And have a lovely day!",
+    welcome: "Hi there, I'm Stefan 👋",
+    subline: "I'm an experienced Engineering Leader based in Germany.",
+    avatar: avatarPic,
   };
 
   return {
@@ -34,11 +38,39 @@ export default function Home({ content }: { content: ContentProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
+      <main>
+        <section>
+          <Image src={content.avatar} alt="Stylised avatar of Stefan Hoth" />
           <h1>{content.welcome}</h1>
+          <hr />
           <p>{content.subline}</p>
-        </div>
+        </section>
+        <section>
+          <h1>
+            Enabling everyone to do their best work and feeling great about it
+          </h1>
+          <p>
+            I want to work with a team of kind, positive, collaborative, and
+            curious people.
+            <br />
+            I want them to feel safe, respected, and appreciated.
+            <br />
+            I want them to feel like their experience, interests, and ambitions
+            matter.
+            <br />I want to enable them to create a meaningful impact - at a
+            sustainable pace.
+          </p>
+          <p>
+            These values and beliefs fuel my contributions to work policies,
+            career frameworks, and process improvements.
+          </p>
+        </section>
+        <section>
+          <div id="contact">
+            <h1>Contact me</h1>
+            <p>TODO</p>
+          </div>
+        </section>
       </main>
     </>
   );
