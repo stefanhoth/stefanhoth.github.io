dbeeaf9440d291dda3524231db9fa530901ec75e
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2025-12-27 17:41:11 +0100

    feat: Add React and Tailwind CSS support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
   output: 'static',
   site: 'https://stefanhoth.com',
   base: '/',
-});
 
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
